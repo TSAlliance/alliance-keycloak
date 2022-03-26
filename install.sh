e1bdca5311e07c30ca782b56bf8afe4f1a0dfe41
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2022-03-26 16:24:03 +0100

    updated

--- install.sh
+++ install.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 sudo read -p "Enter Docker-Compose V2 Version [2.2.3]: " composeVersion
+sudo read -p "Enter Container name [alliance_keycloak]: " container
+sudo read -p "Enter Image name [alliance_keycloak]: " image
+
+
 sudo read -p "Enter Keycloak root username: " username
 sudo read -p "Enter Keycloak root password: " password
 
-sudo read -p "Enter MySQL-Host [localhost]: " dbHost
-sudo read -p "Enter MySQL-Port [3306]: " dbPort
-sudo read -p "Enter MySQL-User [root]: " dbUser
-sudo read -p "Enter MySQL-Password [root]: " dbPass
-sudo read -p "Enter MySQL-Database [keycloak]: " dbName
+#sudo read -p "Enter MySQL-Host [localhost]: " dbHost
+#sudo read -p "Enter MySQL-Port [3306]: " dbPort
+#sudo read -p "Enter MySQL-User [root]: " dbUser
+#sudo read -p "Enter MySQL-Password [root]: " dbPass
+#sudo read -p "Enter MySQL-Database [keycloak]: " dbName
 
 composeVersion=${composeVersion:-2.2.3}
-dbHost=${dbHost:-localhost}
-dbPort=${dbPort:-3306}
-dbUser=${dbUser:-root}
-dbPass=${dbPass:-root}
-dbName=${dbName:-keycloak}
+container=${container:-alliance_keycloak}
+image=${image:-alliance_keycloak}
+
+
+#dbHost=${dbHost:-localhost}
+#dbPort=${dbPort:-3306}
+#dbUser=${dbUser:-root}
+#dbPass=${dbPass:-root}
+#dbName=${dbName:-keycloak}
 
 echo "You selected version $composeVersion for docker-compose V2"
 
@@ -26,13 +34,16 @@ sudo apt upgrade -y
 sudo apt-get install ca-certificates curl gnupg lsb-release tee sed
 
 # Update root username and password
-sudo sed -e "s/*KEYCLOAK_USER*/- KEYCLOAK_USER=$username/" docker-compose.yml
-sudo sed -e "s/*KEYCLOAK_PASSWORD*/- KEYCLOAK_PASSWORD=$username/" docker-compose.yml
-sudo sed -e "s/*DB_ADDR*/- DB_ADDR=$dbHost/" docker-compose.yml
-sudo sed -e "s/*DB_PORT*/- DB_PORT=$dbPort/" docker-compose.yml
-sudo sed -e "s/*DB_DATABASE*/- DB_DATABASE=$dbName/" docker-compose.yml
-sudo sed -e "s/*DB_USER*/- DB_USER=$dbUser/" docker-compose.yml
-sudo sed -e "s/*DB_PASSWORD*/- DB_PASSWORD=$dbPass/" docker-compose.yml
+#sudo sed -e "s/*KEYCLOAK_USER*/- KEYCLOAK_USER=$username/" docker-compose.yml
+#sudo sed -e "s/*KEYCLOAK_PASSWORD*/- KEYCLOAK_PASSWORD=$username/" docker-compose.yml
+#sudo sed -e "s/*DB_ADDR*/- DB_ADDR=$dbHost/" docker-compose.yml
+#sudo sed -e "s/*DB_PORT*/- DB_PORT=$dbPort/" docker-compose.yml
+#sudo sed -e "s/*DB_DATABASE*/- DB_DATABASE=$dbName/" docker-compose.yml
+#sudo sed -e "s/*DB_USER*/- DB_USER=$dbUser/" docker-compose.yml
+#sudo sed -e "s/*DB_PASSWORD*/- DB_PASSWORD=$dbPass/" docker-compose.yml
+
+# Update container name in compose
+sudo sed -e "s/*container_name*/container_name: $container/" docker-compose.yml
 
 # Add GPG Key for docker repo
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
@@ -63,10 +74,17 @@ sudo chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 mkdir cert
 sudo cp $fullchain cert/fullchain.pem
 sudo cp $privKey cert/privkey.pem
-sudo chown $USER:$USER -R cert
+sudo chown $USER:$USER -R cert/*
 
 # Build keycloak image
-docker build -t keycloak .
+docker build -t $image .
+docker compose up -d
+sleep 5
+docker container stop $container
+
+# Configure service file
+sudo sed -e "s/*ExecStart*/ExecStart=docker container start $container/" keycloak.service
+sudo sed -e "s/*ExecStop*/ExecStop=docker container stop $container/" keycloak.service
 
 # Copy service to systemd and enable startup
 sudo cp keycloak.service /etc/systemd/system/keycloak.service
